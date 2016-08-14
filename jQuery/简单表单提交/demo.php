05ccc48cee260f36cbb1fee3138fdc0f67843260
rubbish259 <rubbish_2592@163.com>
2016-08-14 12:52:14 +0800

    简单

---
+++ jQuery/简单表单提交/demo.php
@@ -0,0 +1,14 @@
+<?php
+
+//    $username = $_GET["user"];
+//    $password = $_GET["key"];
+	$username = $_POST["user"];
+	$password = $_POST["key"];
+
+	if ($username == 'abc' && $password == '123123') {
+		echo '<script>location.href="../form/main.html"</script>';
+	} else {
+		echo 'µ«¬º ß∞‹£¨«Î÷ÿ ‘<a href="../form/wbc5.html">∑µªÿ</a>';
+	}
+
+?>
